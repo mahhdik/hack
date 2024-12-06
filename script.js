1dcf71b50ea04b69bc391b3ad738b3ef25b156c5
mahhdik <mahhdik@gmail.com>
2024-12-06 13:47:10 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function doSomthing(){
+    document.getElementById("Translate").innerHTML = "We are in peace"
+}
+function doSomthing1(){
+    document.getElementById("Translate").innerHTML = "We are going to attack you"
+}
+function doSomthing2(){
+    document.getElementById("Translate").innerHTML = "There is food down here!"
+}
